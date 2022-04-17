20dfe62da5e2ef5032ceb2ff9ad13c527a348149
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-04-17 16:05:33 -0400

    Basic boilerplate with minor customization completed

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const app = express();
+const PORT = process.env.PORT || 3333;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connnect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'digitalDreams',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// const helpers = require('./utils/halpers');
+
+// const hbs = exphbs.create({ helpers });
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Sequelize server now listening.'))
+})
